7cba567a86d377386728b11ef8929b1e15e3b9c2
karan2144 <119487411+karan2144@users.noreply.github.com>
2023-04-16 18:50:52 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,3 +1,3 @@
 DATABASE_URI ="sqlite:///database.db" #Database URI
-TOKEN = "ADD YOU BOT TOKEN" #Bot Token 
+TOKEN = "6026463605:AAFFni-vMmIKsMpH7T02LINBY8ulSGmeo-A" #Bot Token 
 BASE_URL ="https://www.1secmail.com/api/v1/" #API Base URL
